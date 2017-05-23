602e47f12f57c92dee80208a2181ff1f7a493dd0
Mudit Mishra <mudit.mishra@sas.com>
2017-05-23 16:42:58 +1000

    added valid configuration check

--- healthcheck.py
+++ healthcheck.py
@@ -1,6 +1,12 @@
 import json
 import socket
 from datetime import datetime
+import os
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log = logging.getLogger('healthcheck')
 
 def webAppAvailability(name):
     status={}
@@ -18,98 +24,96 @@ def getHostName():
         name=socket.gethostbyaddr(socket.gethostname())[0]
     return name
 
-def validateConfig(configFile):
+def configValid(configFile):
+        log = logging.getLogger('configValid')
+        CONFIG_LEVEL1_MUST_KEYS=['env']
+        CONFIG_LEVEL2_MUST_KEYS=['applications']
+        CONFIG_LEVEL3_MUST_KEYS=['protocol','hosts','port','user','password','apps','type','enabled']
+        CONFIG_KEY_FOUND=[]
+        INVALID_CONFIG_FILE=False
+        FOUND_ENV=False
+        FOUND_APPLICATIONS=False
 
-    CONFIG_LEVEL1_MUST_KEYS=['env']
-    CONFIG_LEVEL2_MUST_KEYS=['settings']
-    CONFIG_LEVEL3_MUST_KEYS=['protocol','hosts','port','user','password','apps','type']
-    CONFIG_KEY_FOUND=[]
-    INVALID_CONFIG_FILE=False
-    FOUND_ENV=False
-    FOUND_SETTINGS=False
-    
-    if configFileExist(configFile):
-    
-        try:
-              log.info("Validating configuration file %s" % (configFile))
-              with open(configFile) as input_json:
-                #Load configuration
-                config = json.load(input_json)
-              
-              for config_env in config.keys(): #Iterate environments in configuration file
-                     """
-                     In TOP section
-                     """
-                     if config_env in CONFIG_LEVEL1_MUST_KEYS and not FOUND_ENV :
-                           FOUND_ENV=True
-                           for config_env_item in config[config_env]: #Iterate Environment attributes
-                                 """
-                                 In TOP > env
-                                 """
-                                 log.info("Checking settings for Environment: %s  Level: %s" % (config_env_item['name'],config_env_item['level']))
-                                 FOUND_SETTINGS=False
-                                 for config_env_key in config_env_item:
-                                        """
-                                        In TOP > env > 'environment name' 
-                                        """
-                                        
-                                        if config_env_key in CONFIG_LEVEL2_MUST_KEYS and not FOUND_SETTINGS: #Look for 'SETTINGS' key 
-                                              """
-                                              In TOP > env > 'environment name' > Settings
-                                              """
-                                              FOUND_SETTINGS=True #Key found
-                                              log.debug("Environment Key Name %s" % (config_env_key))
-                                              for config_env_settings_key in config_env_item[config_env_key]:
-                                                    """
-                                                    In TOP > env > 'environment name' > Settings > values'
-                                                    """
-                                                    log.debug("Settings %s" % (config_env_settings_key))
-                                                    
-                                                    """
-                                                    Check if 'must' keys exist in settings - if not then break
-                                                    """
-                                                    for key in config_env_settings_key: #Iterate each key in settings
-                                                          if key in CONFIG_LEVEL3_MUST_KEYS:
-                                                                CONFIG_KEY_FOUND.append(key)
-                                                          
-                                                    log.debug("Number of keys found %d" % (len(CONFIG_KEY_FOUND)))
-                                              
-                                                    """
-                                                    if atleast one 'must' keys not found then break from the loop
-                                                    and declare configuration file as invalid
-                                                    """
-                                                    if not (len(CONFIG_KEY_FOUND) == len(CONFIG_LEVEL3_MUST_KEYS)):
-                                                          INVALID_CONFIG_FILE=True
-                                                          break
-                                                    
-                                                    CONFIG_KEY_FOUND=[]
-                                              
-                                        else:
-                                              continue
-                                 if INVALID_CONFIG_FILE:
-                                       log.info("Failed to validate Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
-                                 else:
-                                       log.info("Successfully validated Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
-                           break
-                     else:
-                           continue
-                                                    
-                                        
-                         
-                     
-        except Exception,e:
-               INVALID_CONFIG_FILE=True
-               log.error('Something went wrong while reading web configuration json file %s' % (configFile))
-               log.error(e,exc_info=True)
-    else:
-      log.info("Configuration File %s does not exist" % (configFile))
-      
-    
-    if INVALID_CONFIG_FILE:
-          return False
-    else:
-          return True
-        
+        if os.path.exists(configFile):
+
+                try:
+                      log.info("Validating configuration file %s" % (configFile))
+                      with open(configFile) as f:
+                          config = json.load(f)
+
+                      for config_env in config.keys(): #Iterate environments in configuration file
+                             """
+                             In TOP section
+                             """
+
+                             if config_env in CONFIG_LEVEL1_MUST_KEYS and not FOUND_ENV :
+                                   FOUND_ENV=True
+                                   for config_env_item in config[config_env]: #Iterate Environment attributes
+                                         """
+                                         In TOP > env
+                                         """
+                                         log.info("Checking settings for Environment: %s  Level: %s" % (config_env_item['name'],config_env_item['level']))
+                                         FOUND_SETTINGS=False
+                                         for config_env_key in config_env_item:
+                                                """
+                                                In TOP > env > 'environment name'
+                                                """
+
+                                                if config_env_key in CONFIG_LEVEL2_MUST_KEYS and not FOUND_APPLICATIONS: #Look for 'SETTINGS' key
+                                                      """
+                                                      In TOP > env > 'environment name' > Settings
+                                                      """
+                                                      FOUND_APPLICATIONS=True #Key found
+                                                      log.debug("Environment Key Name %s" % (config_env_key))
+                                                      for config_env_applications_key in config_env_item[config_env_key]:
+                                                            """
+                                                            In TOP > env > 'environment name' > Applications > values'
+                                                            """
+                                                            log.debug("Application %s" % (config_env_applications_key))
+
+                                                            """
+                                                            Check if 'must' keys exist in settings - if not then break
+                                                            """
+                                                            for key in config_env_applications_key: #Iterate each key in settings
+                                                                  if key in CONFIG_LEVEL3_MUST_KEYS:
+                                                                        CONFIG_KEY_FOUND.append(key)
+
+                                                            log.debug("Number of keys found %d" % (len(CONFIG_KEY_FOUND)))
+
+                                                            """
+                                                            if atleast one 'must' keys not found then break from the loop
+                                                            and declare configuration file as invalid
+                                                            """
+                                                            if not (len(CONFIG_KEY_FOUND) == len(CONFIG_LEVEL3_MUST_KEYS)):
+                                                                  INVALID_CONFIG_FILE=True
+                                                                  break
+
+                                                            CONFIG_KEY_FOUND=[]
+
+                                                else:
+                                                      continue
+                                         if INVALID_CONFIG_FILE:
+                                               log.info("Failed to validate Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
+                                         else:
+                                               log.info("Successfully validated Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
+                                   break
+                             else:
+                                   continue
+
+                except Exception,e:
+                       INVALID_CONFIG_FILE=True
+                       log.error('Something went wrong while reading web configuration json file %s' % (configFile))
+                       log.error(e,exc_info=True)
+        else:
+            log.error("Configuration File %s does not exist" % (configFile))
+
+        if INVALID_CONFIG_FILE:
+              log.info("Configuration File check Failed")
+              return False
+        else:
+              log.info("Configuration File check Passed")
+              return True
+
 class HealthCheckApplication(object):
     def __init__(self,environment,level,name,type,host,port,protocol):
         self.type=type
@@ -145,12 +149,19 @@ class HealthCheckStatus(object):
 
 class HealthCheckConfig(object):
 
-    def __init__(self,config):
-        self.configFilename=config
+    def __init__(self,configFile):
+        log = logging.getLogger('HealthCheckConfig')
+        self.configFilename=configFile
         self.applications=[]
-        self.initialize(config)
+        if configValid(configFile):
+            with open(configFile) as f:
+                config_data=json.load(f)
+                self.initialize(config_data)
+        else:
+            log.info("Stop executing as Configuration File %s is invalid" % (configFile))
 
     def initialize(self,config):
+        log = logging.getLogger('HealthCheckConfig.initialize')
         for config_key in config:
             if config_key == 'env':
                 for environment in config[config_key]:
@@ -180,38 +191,42 @@ class HealthCheckConfig(object):
                                                                          application['protocol']
                                                                          ))
                                     else:
-                                        print "%s is disabled" %(application["Description"])
+                                        log.info("%s is disabled" %(application["Description"]))
 
 class Healthcheck(object):
-        def __init__(self,config):
+        def __init__(self,configFile):
             self.status_dict={}
             self.status_dict["hostname"]=getHostName()
             self.status_dict["timestamp"]=str(datetime.now())
             self.status_dict["output"]=[]
             self.status_output=[]
-            self.hc_config=HealthCheckConfig(config)
+            self.hc_config=HealthCheckConfig(configFile)
+
 
         def getStatus(self):
+            log = logging.getLogger('Healthcheck.getStatus')
             status_output=[]
-
-            for application in self.hc_config.applications:
-                if application.type.upper() == 'WEBAPP':
-                    self.status_dict["output"].append(HealthCheckStatus(application.host,application.name,application.type,'Availability','True','20170523','Success','200 OK').asDict())
-                elif application.type.upper() == 'DISK':
-                    self.status_dict["output"].append(HealthCheckStatus(application.host,application.name,application.type,'Availability','True','20170523','Success','Responding').asDict())
-                else:
-                    print "Invalid Application Type"
+            if self.hc_config.applications:
+                for application in self.hc_config.applications:
+                    if application.type.upper() == 'WEBAPP':
+                        self.status_dict["output"].append(HealthCheckStatus(application.host,application.name,application.type,'Availability','True','20170523','Success','200 OK').asDict())
+                    elif application.type.upper() == 'DISK':
+                        self.status_dict["output"].append(HealthCheckStatus(application.host,application.name,application.type,'Availability','True','20170523','Success','Responding').asDict())
+                    else:
+                        log.info("Invalid Application Type")
+            else:
+                log.info("No applications loaded")
 
         def save(self,filename):
-            print json.dumps(self.status_dict,indent=4)
-            print 'Saving to File %s' % (filename)
+            log = logging.getLogger('Healthcheck.save')
+            if not self.status_dict:
+                log.info(json.dumps(self.status_dict,indent=4))
+                log.info('Saving to File %s' % (filename))
 
-with open('myconfig.json') as data_file:
-     #hc = Healthcheck(data_file)
-     data=json.load(data_file)
-     hc = Healthcheck(data)
-     hc.getStatus()
-     hc.save('f')
+
+hc = Healthcheck(os.path.abspath('config.json'))
+hc.getStatus()
+hc.save('f')
 
 
 #hc.display()
