e9ea1b43db2d771f8514f20f62f1217383fffc6b
Mudit Mishra <mudit.mishra@sas.com>
2017-05-26 10:24:42 +1000

    configcheckupdate

--- config.py
+++ config.py
@@ -5,17 +5,23 @@ import os
 
 def validateConfig(configFile):
         log = logging.getLogger('configValid()')
-        CONFIG_LEVEL1_MUST_KEYS=['env']
+        CONFIG_TOP_KEYS=['env']
+        CONFIG_TOP_ENVKEY='ENV'
         """
-        env is a list with the following dictionary items
+        env is a list [] with the following dictionary items
         """
-        CONFIG_LEVEL2_MUST_KEYS=['applications','name','level']
+        CONFIG_ENVKEYS=['applications','name','level']
+        CONFIG_APPLICATION_KEY='APPLICATIONS'
         """
-        applications is a list with the following dictionary items
+        applications is a list [] with the following dictionary items
         """
-        CONFIG_LEVEL3_MUST_KEYS=['protocol','hosts','port','user','password','apps','type','enabled']
+        CONFIG_APPLICATIONKEYS=['protocol','hosts','port','user','password','apps','type','enabled']
 
-        CONFIG_KEY_FOUND=[]
+
+        CONFIG_TOP_KEYCHECK_RESULT=[]
+        CONFIG_ENV_KEYCHECK_RESULT=[]
+        CONFIG_APPLICATIONS_KEYCHECK_RESULT=[]
+
         INVALID_CONFIG_FILE=False
         FOUND_ENV=False
         FOUND_APPLICATIONS=False
@@ -31,64 +37,65 @@ def validateConfig(configFile):
                              """
                              In TOP section
                              """
+                             log.info(config_env)
+                             if config_env.upper() == CONFIG_TOP_ENVKEY and not FOUND_ENV :
+                                 CONFIG_TOP_KEYCHECK_RESULT.append(config_env)
+                                 FOUND_ENV=True
+                                 """
+                                 In TOP > env
+                                 """
+                                 log.info("config_env_item ----" % config[config_env])
+                                 log.info("Checking settings for Environment: %s  Level: %s" % (config[config_env]['name'],config[config_env]['level']))
+                                 FOUND_APPLICATIONS=False
 
-                             if config_env in CONFIG_LEVEL1_MUST_KEYS and not FOUND_ENV :
-                                   FOUND_ENV=True
-                                   for config_env_item in config[config_env]: #Iterate Environment attributes
-                                         """
-                                         In TOP > env
-                                         """
-                                         log.debug("Checking settings for Environment: %s  Level: %s" % (config_env_item['name'],config_env_item['level']))
-                                         FOUND_APPLICATIONS=False
-                                         for config_env_key in config_env_item:
+                                 for config_app_key in config[config_env]:
+                                        """
+                                        In TOP > env > 'items'
+                                        """
+                                        if config_app_key in CONFIG_ENVKEYS:
+                                            CONFIG_ENV_KEYCHECK_RESULT.append(config_app_key)
+                                            if config_app_key.upper() == CONFIG_APPLICATION_KEY:
+                                                #process application list
                                                 """
-                                                In TOP > env > 'environment name'
+                                                In TOP > env > 'Applications'
                                                 """
+                                                for applications in config[config_env][config_app_key]:
+                                                    #log.info(applications)
+                                                    CONFIG_APPLICATIONS_KEYCHECK_RESULT=[]
+                                                    for applications_key in applications.keys():
+                                                        if applications_key in CONFIG_APPLICATIONKEYS:
+                                                            log.info(applications_key)
+                                                            CONFIG_APPLICATIONS_KEYCHECK_RESULT.append(applications_key)
 
-                                                if config_env_key in CONFIG_LEVEL2_MUST_KEYS and not FOUND_APPLICATIONS: #Look for 'Applications' key
-                                                      """
-                                                      In TOP > env > 'environment name' > Applications
-                                                      """
-                                                      FOUND_APPLICATIONS=True #Key found
-                                                      for config_env_applications_key in config_env_item[config_env_key]:
-                                                            """
-                                                            In TOP > env > 'environment name' > Applications > values'
-                                                            """
-                                                            log.debug("Application ** %s **" % (config_env_applications_key['Description']))
+                                                    log.debug("Number of valid Application keys %d" % (len(CONFIG_APPLICATIONKEYS)))
+                                                    log.debug("Number of Application keys found %d" % (len(CONFIG_APPLICATIONS_KEYCHECK_RESULT)))
 
-                                                            """
-                                                            Check if 'must' keys exist in settings - if not then break
-                                                            """
-                                                            for key in config_env_applications_key: #Iterate each key in settings
-                                                                  if key in CONFIG_LEVEL3_MUST_KEYS:
-                                                                        CONFIG_KEY_FOUND.append(key)
+                                                    if not (len(CONFIG_APPLICATIONS_KEYCHECK_RESULT) == len(CONFIG_APPLICATIONKEYS)):
+                                                        log.debug("Number of Application keys did not match")
+                                                        log.debug("*** INVALID CONFIGURATION FILE ***")
+                                                        INVALID_CONFIG_FILE=True
+                                                        break
 
-                                                            log.debug("Number of valid keys %d" % (len(CONFIG_LEVEL3_MUST_KEYS)))
-                                                            log.debug("Number of keys found %d" % (len(CONFIG_KEY_FOUND)))
+                                            else:
+                                                pass
 
-                                                            """
-                                                            if atleast one 'must' keys not found then break from the loop
-                                                            and declare configuration file as invalid
-                                                            """
-                                                            if not (len(CONFIG_KEY_FOUND) == len(CONFIG_LEVEL3_MUST_KEYS)):
-                                                                  log.debug("Number of keys did not match")
-                                                                  log.debug("*** INVALID CONFIGURATION FILE ***")
-                                                                  INVALID_CONFIG_FILE=True
-                                                                  break
-                                                            else:
-                                                                log.debug("Number of keys matched")
+                                 if not (len(CONFIG_ENV_KEYCHECK_RESULT) == len(CONFIG_ENVKEYS)):
+                                     log.debug("Number of environment keys did not match")
+                                     INVALID_CONFIG_FILE=False
 
-                                                            CONFIG_KEY_FOUND=[]
 
-                                                else:
-                                                      continue
-                                         if INVALID_CONFIG_FILE:
-                                               log.debug("Failed to validate Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
-                                         else:
-                                               log.debug("Successfully validated Environment: %s  Level: %s " % (config_env_item['name'],config_env_item['level']))
-                                   break
+
+
+                                 #if INVALID_CONFIG_FILE:
+                                 #      log.info("Failed to validate Environment: %s  Level: %s " % (config[config_env]['name'],config[config_env]['level']))
+                                 #else:
+                                        #log.info("Successfully validated Environment: %s  Level: %s " % (config[config_env]['name'],config[config_env]['level']))
+
+                                  #ENV found, now break from IF condition
+                                 break
                              else:
-                                   continue
+                                 log.debug("Looking for env key")
+                                 continue
 
                 except Exception,e:
                        INVALID_CONFIG_FILE=True
