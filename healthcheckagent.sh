cbf3f1b4dd2527e3bff3660434395fbbb770632a
Mudit Mishra <mudit.mishra@sas.com>
2017-06-07 13:22:53 +1000

    change path

--- healthcheckagent.sh
+++ healthcheckagent.sh
@@ -2,11 +2,11 @@
 
 SCRIPT_PATH=$(cd `dirname ${0}`; pwd)
 
-PYTHON_MODULES=${SCRIPT_PATH}/..
+PYTHON_MODULES=${SCRIPT_PATH}/python-modules
 
 export PYTHONPATH=${PYTHON_MODULES}/jinja2-2.9.6/lib:\
 ${PYTHON_MODULES}/markupsafe/lib:\
-${PYTHON_MODULES}/fabric/lib:\
+${PYTHON_MODULES}/Fabric-1.13.2/lib:\
 ${PYTHON_MODULES}/paramiko-1.18.2/lib:\
 ${PYTHON_MODULES}/ecdsa-0.13/lib:\
 ${PYTHON_MODULES}/pycrypto-2.6.1/lib
@@ -19,24 +19,24 @@ AGENT_CMD="python ${SCRIPT_PATH}/healthcheckagent.py"
 # Get argument
 case "$1" in
    start | -start)
-   
+
           $AGENT_CMD start
-          
+
          ;;
    stop | -stop)
-   
+
          $AGENT_CMD stop
-         
+
          ;;
    status | -status)
-   
+
          $AGENT_CMD status
-         
-         ;; 
+
+         ;;
    restart | -restart)
-   
+
          $AGENT_CMD restart
-         
+
          ;;
    *)
          echo "Usage: $SCRIPT {-}{start|stop|status|restart}"
