5daf240283f4d27a7e86cc759d2c6fbee2234a8c
Mudit Mishra <mudit.mishra@sas.com>
2017-06-29 11:07:50 +1000

    bin updates

--- bin/healthcheckall.sh
+++ bin/healthcheckall.sh
@@ -2,9 +2,8 @@
 
 #https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 BINPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export HC_CONFIG_PATH=${BINPATH}/../conf.d/reporteverything.cfg
 
-export HC_CONFIG_FILE=${BINPATH}/../conf.d/all7am.cfg
+${BINPATH}/healthcheckagent.sh "$@"
 
-${BINPATH}/healthcheckagent.sh start
-
-echo "send full report"
+exit $?
