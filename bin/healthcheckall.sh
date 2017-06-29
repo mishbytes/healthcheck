8dbc1afe01955d42cf59e20ff3baa136e7bb37fe
Mudit Mishra <mudit.mishra@sas.com>
2017-06-29 11:09:03 +1000

    rename file

--- bin/healthcheckall.sh
+++ bin/healthcheckall.sh
@@ -2,7 +2,7 @@
 
 #https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 BINPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export HC_CONFIG_PATH=${BINPATH}/../conf.d/reporteverything.cfg
+export HC_CONFIG_PATH=${BINPATH}/../conf.d/reportall.cfg
 
 ${BINPATH}/healthcheckagent.sh "$@"
 
